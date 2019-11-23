a40ed8171b227214f2baa5af60cc9edb9be0e074
Geraldo Domingos <geraldo.dsn2000@gmail.com>
2019-11-23 18:52:26 -0300

    possui alguns erros

--- EJS/index.js
+++ EJS/index.js
@@ -1,27 +1,13 @@
 const express = require('express')
-const fs = require('fs')
+const path = require('path')
 
 const app = express()
 
-const pessoa = {
-    id: 871286346182634,
-    name: 'Geraldo Domingos',
-    phoneNumber: 81727372823
-}
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
 
 app.get('/', (request, response) => {
-
-    fs.readFile('./public/index.html', 'utf8', (err, data) => {
-        if (!err) {
-
-            data = data.replace('Nome', `Nome: ${pessoa.name}`)
-                .replace('numero', `Numero: ${pessoa.phoneNumber}`)
-
-
-            response.send(data)
-        }
-    })
-
+    app.render(user)
 })
 
 
